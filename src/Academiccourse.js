8609cd3fd129daa75ec1b1859ba68eee7ae439a1
Kotasaigoutham89 <Kotasaigoutham89>
2022-02-23 21:40:03 +0530

    ACademy

--- src/Academiccourse.js
+++ src/Academiccourse.js
@@ -5,9 +5,58 @@ function Academiccourse() {
 
     return (
         <>
-           <div>
-               Academiccourse
-           </div>
+            <div className="main_container" style={{ backgroundColor: "rgb(223,246,221)" }}>
+                <div className='course-container'>
+                    
+                    <div className='academiccourse-items'>
+                    <div className='course-heading'>
+                        Academic Course
+                    </div>
+                        <div className='items'>
+                            <div>
+                                Sustainability Course Title
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <div className='items'>
+                            <div>
+                                Department
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <div className='items'>
+                            <div>
+                                Level of Course
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <div className='items'>
+                            <div>
+                                Brief  Course Description
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <div className='items'>
+                            <div>
+                                Course Type
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <div className='items'>
+                            <div>
+                                Semester Offered
+                            </div>
+                            <input type="text" />
+                        </div>
+                        <div className='items'>
+                            <div>
+                                Year Offered
+                            </div>
+                            <input type="text" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
